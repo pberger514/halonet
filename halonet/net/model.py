3f590366c26b9b3bae8cff548b3c7c181043a461
Philippe Berger <philippe.j.berger@gmail.com>
2017-12-10 19:56:07 -0500

    Making some progress on the model.

--- halonet/net/model.py
+++ halonet/net/model.py
@@ -4,10 +4,10 @@
 # Authors: P. Berger, G. Stein
 #
 # References: 
-# - V-Net https://arxiv.org/abs/1606.04797
-# - V-Net in pytorch https://github.com/mattmacy/vnet.pytorch
-# - Cosmology https://arxiv.org/pdf/1711.02033.pdf
-# - YOLO (You Only Look Once)?
+# [1] V-Net https://arxiv.org/abs/1606.04797
+# [2] V-Net in pytorch https://github.com/mattmacy/vnet.pytorch
+# [3] Cosmology https://arxiv.org/pdf/1711.02033.pdf
+# [4] YOLO (You Only Look Once)?
 # 
 #
 
@@ -23,35 +23,29 @@
 
 import keras.models as km
 import keras.layers as kl
+from keras.layers.merge import add
 
 
 #######################################################################
 
 
-def get_model(filters=[2, 10], numfm, numnodes, input_shape = (64, 64, 64, 1),
-              output_size = (64, 64, 64)):
+def get_model(nlevels, nfmi = 16, input_shape = (64, 64, 64, 1)):
 
     """
     Returns a CNN V-Net Keras model.
 
     Parameters
     -----------
-        filters: list
-            Number of layers/filter banks to use.
+        nlevels: list
+            Number of levels in the V-Net, which is the number of times the resolution of
+            of the filter is halved.
 
-        numfm: int 
-            the number of feature maps in the convolution layer
-
-        numnodes: int
-            the number of nodes in the fully-connected layer
+        nfmi: int 
+            The number of feature maps in the first convolutional layer (default = 16).
 
         intput_shape: tuple, 
             the shape of the input data, 
             default = (64, 64, 64, 1)
-
-        output_size: int, 
-            the number of nodes in the output layer,
-            default = 10.
 
     Output
     -------
@@ -61,35 +55,61 @@ def get_model(filters=[2, 10], numfm, numnodes, input_shape = (64, 64, 64, 1),
     """
 
     # Initialize the model.
-    model = km.Sequential()
+    #model = km.Sequential()
+    #This is not a Sequential model so create input state
+    input_state = kl.Input(shape = input_shape)
 
-    for fi, filt in enumerate(filters):
-        # Add a 2D convolution layer, with numfm feature maps.
+    #These are hard coded following [1]
+    conv_kernel_size = (5, 5, 5)
+    updown_kernel_size = (2, 2, 2)
+    updown_stride = updown_kernel_size
+
+    level_list = []
+
+    for fi in range(nlevels):
+        # Add the initial convolution layer, with nfm feature maps.
+        # For simplicity we are doing only one convolution per level (see [1]).
         if fi == 0:
-            model.add(kl.Conv3D(numfm, kernel_size = (filt, filt, filt),
-                                input_shape = input_shape,
-                                activation = 'relu'))
+            residual = kl.Conv3D(nfm, kernel_size = conv_kernel_size, padding='same')(input_state)
+            shortcut = kl.Conv3D(nfm, kernel_size = (1, 1, 1), padding='same')(input_state)
+
         else:
-            model.add(kl.Conv3D(numfm, kernel_size = (filt, filt, filt),
-                                activation = 'relu'))
+            residual = kl.Conv3D(nfm*2**fi, kernel_size = conv_kernel_size, padding='same')(x)
+            shortcut = kl.Conv3D(nfm*2**fi, kernel_size = (1, 1, 1), padding='same')(x)
 
-        # Add an average pooling layer.
-        #model.add(kl.AvergePooling3D(pool_size = (2, 2, 2),
-        #                             strides = (2, 2, 2)))
+        # Perform elementwise sum with input to train on residuals.
+        x = add([residual, shortcut])
 
+        #Save a copy for fine-grained features forwarding
+        level_list.append(add([residual, shortcut]))
 
-    # Convert the network from 3D to 1D.
-    #model.add(kl.Flatten())
+        # Peform a down convolution with PReLU activation
+        x = kl.Conv3D(nfm, kernel_size = updown_kernel_size, strides=updown_stride,
+                      activation = kl.PReLU, padding='same')(x) #padding?
 
-    # Add a fully-connected layer.
+        # Check average pooling vs. residual network?
+
+    # Add a fully-connected layer?
     #model.add(kl.Dense(numnodes, activation = 'tanh'))
 
-    for fi, filt in enumerate(filters[::-1]):
-        # Add a 2D convolution layer, with numfm feature maps.
-        model.add(kl.Conv3DTranspose(numfm, kernel_size = (filt, filt, filt),
-                                     activation = 'relu'))
+    # Step back up to achieve initial resolution
+    for fi in range(nlevels)[::-1]:
+        
+        if fi != nlevels:
+            #Concatenate with fine grained forwarded features along filters axis
+            x = kl.Concatenate(axis=-1)([level_list[fi], x])
 
+        #Perform a couple convolutions
+        residual = kl.Conv3D(nfm*2**(fi-1), kernel_size = conv_kernel_size, padding='same')(x)
+        residual = kl.Conv3D(nfm*2**(fi-1), kernel_size = conv_kernel_size, padding='same')(residual)
+        shortcut = kl.Conv3D(nfm*2**(fi-1), kernel_size = (1, 1, 1), padding='same')(x)
+        x = add([residual, shortcut])
+            
+        # Peform an up-convolution with PReLU activation
+        x = kl.Conv3DTranspose(nfm, kernel_size = updown_kernel_size, strides=updown_stride,
+                               activation = kl.PReLU)(x)
 
-    # Return the model.
+    
+    model = km.Model(inputs = input_state, outputs = x)
+
     return model
-
