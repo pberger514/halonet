f812621f787e015573a8c1550ade3207429b4ddb
Philippe Berger <philippe.j.berger@gmail.com>
2017-12-11 01:56:12 -0500

    Added Dice loss function, seems to compile.

---
+++ halonet/net/loss.py
@@ -0,0 +1,6 @@
+from keras import backend as K
+from keras.utils.generic_utils import serialize_keras_object
+
+def dice_loss_coefficient(y_true, y_pred):
+
+    return 2*K.sum(y_true*y_pred, axis=-1) / K.sum(K.square(y_true) + K.square(y_pred), axis=-1)
